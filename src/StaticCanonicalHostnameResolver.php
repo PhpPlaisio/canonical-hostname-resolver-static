8a511983be27b8eeab2e8c07f051b2701b338b74
Paul Water <p.r.water@setbased.nl>
2017-06-01 12:27:05 +0200

    Initial commit.

---
+++ src/StaticCanonicalHostnameResolver.php
@@ -0,0 +1,54 @@
+<?php
+//----------------------------------------------------------------------------------------------------------------------
+namespace SetBased\Abc\CanonicalHostnameResolver;
+
+//----------------------------------------------------------------------------------------------------------------------
+/**
+ * A CanonicalHostnameResolver with static hostname.
+ */
+class StaticCanonicalHostnameResolver implements CanonicalHostnameResolver
+{
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * The canonical host name.
+   *
+   * @var string|null
+   */
+  private $canonicalHostname;
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Object constructor.
+   *
+   * @param string $hostname The canonical hostname.
+   *
+   * @api
+   * @since 1.0.0
+   */
+  public function __construct($hostname)
+  {
+    // Remove port number, if any.
+    $p = strpos($hostname, ':');
+    if ($p!==false) $hostname = substr($hostname, 0, $p);
+
+    $this->canonicalHostname = strtolower(trim($hostname));
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Returns the canonical hostname (a.k.a. preferred fully qualified domain name).
+   *
+   * @return string
+   *
+   * @api
+   * @since 1.0.0
+   */
+  public function getCanonicalHostname()
+  {
+    return $this->canonicalHostname;
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+}
+
+//----------------------------------------------------------------------------------------------------------------------
---
+++ test/StaticCanonicalHostnameResolverTest.php
@@ -0,0 +1,61 @@
+<?php
+//----------------------------------------------------------------------------------------------------------------------
+namespace SetBased\Abc\CanonicalHostnameResolver\Test;
+
+use PHPUnit\Framework\TestCase;
+use SetBased\Abc\CanonicalHostnameResolver\StaticCanonicalHostnameResolver;
+
+//----------------------------------------------------------------------------------------------------------------------
+/**
+ * Test cases for class StaticCanonicalHostnameResolver.
+ */
+class StaticCanonicalHostnameResolverTest extends TestCase
+{
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Test method getCanonicalHostname without port number.
+   */
+  public function testGetDomain1a()
+  {
+    $resolver = new StaticCanonicalHostnameResolver('www.example.com');
+
+    $this->assertSame('www.example.com', $resolver->getCanonicalHostname());
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Test method getCanonicalHostname without port number anf host name in upper case.
+   */
+  public function testGetDomain1b()
+  {
+    $resolver = new StaticCanonicalHostnameResolver('www.EXAMPLE.COM');
+
+    $this->assertSame('www.example.com', $resolver->getCanonicalHostname());
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Test method getCanonicalHostname with trailing and leading whitespace.
+   */
+  public function testGetDomain1c()
+  {
+    $resolver = new StaticCanonicalHostnameResolver(" www.example.com\t\n\r");
+
+    $this->assertSame('www.example.com', $resolver->getCanonicalHostname());
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Test method getCanonicalHostname with port number
+   */
+  public function testGetDomain2()
+  {
+    $resolver = new StaticCanonicalHostnameResolver('www.example.com:8080');
+
+    $this->assertSame('www.example.com', $resolver->getCanonicalHostname());
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+}
+
+//----------------------------------------------------------------------------------------------------------------------
